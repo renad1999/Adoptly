99a2344ac47354155095497f91a0e97365408eec
KyleBarter <kylebarter4@gmail.com>
2023-06-19 23:11:07 +0100

    skeleton and pseudo commit

---
+++ main_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+#! tuples here
+
+
+#! Models go below.
+
+#? pet model
+#name, species, breed, age, description charfields
+
+
+#? pet prompts as a separate model with a many to many relationship with the pet model
+
+
+#? user model
+#name, lifestyle, preferences charfields
---
+++ main_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+
+#! Create your views here.
+
+#? Login, render request login.html
+
+
+#? Home, render request home.html
+
+
+#? questionnaire, render request questionnaire.html
+
+
+#? pet detail, render request pet_detail.html, id param required
+
+
+#? below for create, update & delete views for both pet and user
+
+
+
