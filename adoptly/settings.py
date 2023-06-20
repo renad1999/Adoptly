076d707360c0220f346cd947061815dabfac947a
Louise Raffray <raffraylouise@gmail.com>
2023-06-20 16:41:56 +0100

    default redirect after login

--- adoptly/settings.py
+++ adoptly/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#? Successful logins are redirected to home.html - Lou
+LOGIN_REDIRECT_URL = '/home/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
