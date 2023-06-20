02681a1424590b38da611323bb01482ac2a35d58
Louise Raffray <raffraylouise@gmail.com>
2023-06-20 16:45:25 +0100

    sign up view function

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 #! Functions
 #? Pawfect matches
     # if score = 3 show pet.name else append
 
+def signup(request): # Sign up function, do not touch! - Lou
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
 
 #! Create your views here.
 
