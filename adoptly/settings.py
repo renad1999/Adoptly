903d7b3db6c0e1a0730e1b644030e5d30ba3188a
KyleBarter <kylebarter4@gmail.com>
2023-06-21 10:15:24 +0100

    databse settings created

--- adoptly/settings.py
+++ adoptly/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'adoptly.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '<DATABASE_NAME>',
+        'USER': '<DATABASE_USER>',
+        'PASSWORD': '<DATABASE_PASSWORD>',
+        'HOST': '<DATABASE_HOST>',
+        'PORT': '5432',
     }
 }
 
