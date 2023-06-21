87389ecd71aada6f4af720f96e9628ccfb29b586
KyleBarter <kylebarter4@gmail.com>
2023-06-21 10:34:59 +0100

    databse not quite finished

--- adoptly/settings.py
+++ adoptly/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from environ import Env
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
